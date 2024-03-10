e2c59c5632ece7fdf8925aef2750429d53e59338
Aaron_Zhou <clark1729@outlook.com>
2024-03-10 22:26:11 +0800

    perf: add workbox-rspack-plugin

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from "@rsbuild/core";
 import { pluginVue2 } from "@rsbuild/plugin-vue2";
-import Icons from "unplugin-icons/webpack";
-import { FileSystemIconLoader } from "unplugin-icons/loaders";
+import { GenerateSW } from "@aaroon/workbox-rspack-plugin";
 
 const { publicVars: vueEnvs } = loadEnv({ prefixes: ["VUE_APP_"] });
 
@@ -68,6 +67,62 @@ export default defineConfig(({ envMode, env }) => {
         resolve: {
           extensions: [".vue", ".js", ".jsx", ".tsx", ".ts", ".json"],
         },
+        plugins: [
+          new GenerateSW({
+            skipWaiting: true,
+            clientsClaim: true,
+            exclude: [/\.txt$/, /\.map$/],
+            // globPatterns: ['**/{main,vendor,index}*.{js,css}'],
+            // globIgnores: ['**/*.html', '**/*.webmanifest'],
+            maximumFileSizeToCacheInBytes: 50 * 1000 * 1000,
+            sourcemap: false,
+            navigateFallback: undefined,
+            cleanupOutdatedCaches: true,
+            runtimeCaching: [
+              {
+                urlPattern: /^https:\/\/carllllo.work\/music.*\.(js|css)$/,
+                handler: "CacheFirst",
+                options: {
+                  cacheName: "app-shell",
+                  cacheableResponse: {
+                    statuses: [200],
+                  },
+                  expiration: {
+                    maxAgeSeconds: 30 * 24 * 60 * 60,
+                  },
+                },
+              },
+              {
+                urlPattern: /\.(?:png|gif|jpg|jpeg|webp|svg)$/i,
+                handler: "CacheFirst",
+                options: {
+                  cacheName: "app-images",
+                  cacheableResponse: {
+                    statuses: [200],
+                  },
+                  expiration: {
+                    maxAgeSeconds: 60 * 24 * 60 * 60,
+                    maxEntries: 100,
+                  },
+                },
+              },
+              {
+                urlPattern: /^https:\/\/.*126.net\/.*\.(mp3|flac)$/i,
+                handler: "StaleWhileRevalidate",
+                options: {
+                  cacheName: "music",
+                  cacheableResponse: {
+                    statuses: [200, 206],
+                  },
+                  expiration: {
+                    maxEntries: 50,
+                  },
+                  rangeRequests: true,
+                },
+              },
+            ],
+          }),
+        ],
       },
     },
     performance: {
